fc21b0ab5ee95ca7d1750cc997c1a049aee9bdd1
Michael Ndula <michaelndula@gmail.com>
2023-11-09 22:12:43 +0300

    Adding the Dispatcher Servlet

--- TravelAgencyApp/src/main/java/main/config/ApplicationInitializer.java
+++ TravelAgencyApp/src/main/java/main/config/ApplicationInitializer.java
@@ -1,4 +1,18 @@
 package main.config;
 
-public class ApplicationInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
 }
