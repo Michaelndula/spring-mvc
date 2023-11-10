8b6b49d28706c69a58dd09a6d5b1cbd63f48f3e5
Michael Ndula <michaelndula@gmail.com>
2023-11-10 11:23:22 +0300

    Adding getters and setters to the Model

--- TravelAgencyApp/src/main/java/main/model/Tour.java
+++ TravelAgencyApp/src/main/java/main/model/Tour.java
@@ -16,5 +16,51 @@ public class Tour {
     private int duration;
     private boolean allInclusive;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Continents getContinents() {
+        return continents;
+    }
+
+    public void setContinents(Continents continents) {
+        this.continents = continents;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public boolean isAllInclusive() {
+        return allInclusive;
+    }
+
+    public void setAllInclusive(boolean allInclusive) {
+        this.allInclusive = allInclusive;
+    }
 }
