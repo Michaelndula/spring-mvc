c02a411dec4735cd7198b4be37b5bb28b8a57cd0
Michael Ndula <michaelndula@gmail.com>
2023-11-14 06:05:27 +0300

    Adding display errors on the form

--- TravelAgencyApp/src/main/java/main/controller/HomeController.java
+++ TravelAgencyApp/src/main/java/main/controller/HomeController.java
@@ -1,7 +1,6 @@
 package main.controller;
 
 import main.model.Tour;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
