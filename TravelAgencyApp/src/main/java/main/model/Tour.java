3516440dde10a4d1079dc74c937e66b14237a5dc
Michael Ndula <michaelndula@gmail.com>
2023-11-14 05:34:00 +0300

    Adding validation and regex

--- TravelAgencyApp/src/main/java/main/model/Tour.java
+++ TravelAgencyApp/src/main/java/main/model/Tour.java
@@ -2,17 +2,25 @@ package main.model;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 public class Tour {
     private enum Continents{
         Africa, Asia, Europe, North_America, South_America;
     }
+    @NotBlank(message = "Name cannot be null")
+    @Size(min = 5, message = "name should have at least 5 characters")
     private String name;
+    @Pattern(regexp = "^[a-zA-Z]{2}-[0-9]{2}[a-zA-Z]{1}$", message = "Expected format is CC-DDC where C is character and D is digit")
     private String code;
     private Continents continent;
+    @NotNull(message = "Date cannot be null")
+    @Future(message = "Date should be from the future")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
+    @Min(value = 7, message = "Must be between 7 and 21")
+    @Max(value = 21, message = "Must be between 7 and 21")
     private int duration;
     private boolean allInclusive;
 
