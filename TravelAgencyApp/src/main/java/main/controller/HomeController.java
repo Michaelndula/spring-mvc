636419189bb0b8dedf5b92f6925137dc4450ea6c
Michael Ndula <michaelndula@gmail.com>
2023-11-14 05:40:25 +0300

    Returning the form if with errors

--- TravelAgencyApp/src/main/java/main/controller/HomeController.java
+++ TravelAgencyApp/src/main/java/main/controller/HomeController.java
@@ -4,11 +4,13 @@ import main.model.Tour;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,7 +29,10 @@ public class HomeController {
     }
 
     @PostMapping("/processTour")
-    public String postTour(@ModelAttribute Tour tour){
+    public String postTour(@Valid @ModelAttribute Tour tour, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "form";
+        }
         tours.add(tour);
         return "redirect:showOffer";
     }
