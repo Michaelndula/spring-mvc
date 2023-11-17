96d1b2c3c7fe84c9f4ed7dec6a01808e8491009a
Michael Ndula <michaelndula@gmail.com>
2023-11-17 05:45:28 +0300

    Configuring Hibernate and connecting the database

--- TravelAgencyApp/src/main/java/main/config/ApplicationInitializer.java
+++ TravelAgencyApp/src/main/java/main/config/ApplicationInitializer.java
@@ -9,7 +9,7 @@ public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherSe
     }
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {WebConfig.class};
+        return new Class[] {WebConfig.class, DatabaseConfig.class};
     }
     @Override
     protected String[] getServletMappings() {
